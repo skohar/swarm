990e8383cc26cae29971c678398efc397df1a8cf
skohar <skohar@users.noreply.github.com>
2015-10-01 12:32:17 +0900

    Fix test script

--- circle-test.sh
+++ circle-test.sh
@@ -3,6 +3,6 @@
 set -x
 set -e
 
-docker build -t code-check/codecheck:latest .
+docker build -t eg_sshd .
 docker run -d -P --name test_sshd eg_sshd
 ssh root@localhost -p $(docker port test_sshd 22 | awk -F "[:]" '{print $2}')
