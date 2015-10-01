68a5a7858b29e14bf490f95ba8dcbcea292aea40
skohar <skohar@users.noreply.github.com>
2015-10-01 14:12:59 +0900

    .

--- circle-test.sh
+++ circle-test.sh
@@ -5,4 +5,4 @@ set -e
 
 docker build -t eg_sshd .
 docker run -d -P --name test_sshd eg_sshd
-ssh root@localhost -p $(docker port test_sshd 22 | awk -F "[:]" '{print $2}')
+ansible-playbook -v -i 'localhost,' -u root -p $(docker port test_sshd 22 | awk -F "[:]" '{print $2}') playbook.yml
