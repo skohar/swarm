342d8988096cb523c0b2a5d8a557e396f29b7216
skohar <skohar@users.noreply.github.com>
2015-10-05 11:26:28 +0900

    Fix deployment script

--- circle-test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -x
-set -e
-
-docker build -t eg_sshd .
-docker run -d -P --name test_sshd eg_sshd
-ansible-playbook -v -i 'localhost,' -u root -p $(docker port test_sshd 22 | awk -F "[:]" '{print $2}') playbook.yml
---
+++ deployment_production.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -x
+set -e
+
+instanceIds=$(aws ec2 run-instances --region ap-northeast-1 --image-id ami-936d9d93 --subnet-id subnet-377cc840  --instance-type t2.micro --security-group-id sg-93cd8ef6 --key-name code-check-prod --associate-public-ip-address --count 2 | jq -r '.Instances[] .InstanceId')
+aws ec2 create-tags --region ap-northeast-1 --resources $instanceIds --tags Name=code-check-prod-$CIRCLE_SHA1-$(git rev-parse --short HEAD)
+
+
