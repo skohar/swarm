5d65359553623f342e6b85ee0d8fda233d82f300
skohar <skohar@users.noreply.github.com>
2015-10-09 16:32:49 +0900

    dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 CIRCLECI_CACHE_DIR="${HOME}/bin"
-PACKER_URL="https://dl.bintray.com/mitchellh/packer/packer_0.7.5_linux_amd64.zip"
+PACKER_URL="https://dl.bintray.com/mitchellh/packer/packer_0.8.6_linux_amd64.zip"
 
 if [ ! -f "${CIRCLECI_CACHE_DIR}/packer" ]; then
   wget -O /tmp/packer.zip "${PACKER_URL}"
