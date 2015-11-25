713dea3cce9f4abb0dd71b5a31710ff2850f2efe
skohar <skohar@users.noreply.github.com>
2015-11-25 21:57:15 +0900

    Fix validation failure

---
+++ ami_name.sh
@@ -0,0 +1,6 @@
+#/bin/bash -eu
+name () {
+    DATE_TIME=`date -u +%FT%TZ | sed 's/://g' | sed 's/-//g'`
+    AMI_NAME=whale-$DATE_TIME-$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short HEAD)-$(cd env-builder && git checkout -q master && git rev-parse --abbrev-ref HEAD)-$(cd env-builder && git rev-parse --short HEAD)
+    echo $AMI_NAME
+}
--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 (while true; do echo "Don't DIE!!!!"; sleep 60; done) & # jobs %1
-DATE_TIME=`date -u +%FT%TZ | sed 's/://g' | sed 's/-//g'`
-AMI_NAME=whale-$DATE_TIME-$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short HEAD)-$(cd env-builder && git checkout -q master && git rev-parse --abbrev-ref HEAD)-$(cd env-builder && git rev-parse --short HEAD)
-packer build -var $AMI_NAME packer.json
+source ami_name.sh
+packer build -var "AMI_NAME=name" packer.json
 kill %1
