dcc0e147289190716e1ac2422552479815362e0f
skohar <skohar@users.noreply.github.com>
2015-11-27 14:35:03 +0900

    Handle packer exit code

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 (while true; do echo "Don't DIE!!!!"; sleep 60; done) & # jobs %1
 source ami_name.sh
-packer build -var "AMI_NAME=name" packer.json
+RESULT=`packer build -var "AMI_NAME=name" packer.json`
 kill %1
+exit $RESULT
