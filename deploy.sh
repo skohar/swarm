902eaf64eb582c02f7670c7876659d25bcf52f3b
skohar <skohar@users.noreply.github.com>
2015-12-07 11:40:09 +0900

    Fix numeric argument required

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 (while true; do echo "Don't DIE!!!!"; sleep 60; done) & # jobs %1
 source ami_name.sh
 AMI_NAME=`name`
-RESULT=`packer build -var "AMI_NAME=$AMI_NAME" packer.json`
+packer build -var "AMI_NAME=$AMI_NAME" packer.json
+RESULT=$?
 kill %1
 exit $RESULT
