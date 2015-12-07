f0b4784c80d4c5227f52f621cdad6484c245a6d5
skohar <skohar@users.noreply.github.com>
2015-12-07 17:19:25 +0900

    Replace `` with $()

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -eux
 (while true; do echo "Don't DIE!!!!"; sleep 60; done) & # jobs %1
 source ami_name.sh
-AMI_NAME=`name`
+AMI_NAME=$(name)
 packer build -var "AMI_NAME=$AMI_NAME" packer.json
 RESULT=$?
 kill %1
