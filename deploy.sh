dd3e1c8a2f3e0f2d4def8aaecadb534f48292ed8
skohar <skohar@users.noreply.github.com>
2015-12-07 15:49:22 +0900

    Fix AMI naming problem

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
+#!/bin/bash -eux
 (while true; do echo "Don't DIE!!!!"; sleep 60; done) & # jobs %1
 source ami_name.sh
-RESULT=`packer build -var "AMI_NAME=name" packer.json`
+AMI_NAME=`name`
+RESULT=`packer build -var "AMI_NAME=$AMI_NAME" packer.json`
 kill %1
 exit $RESULT
