65a582ad794a61182749309b1d96c80249b20f04
skohar <skohar@users.noreply.github.com>
2015-12-16 18:11:48 +0900

    parameterization

--- terraform/attach/variable.tf
+++ terraform/attach/variable.tf
@@ -2,13 +2,13 @@ variable "region" {}
 variable "availability_zones" {}
 variable "subnet_id" {}
 
-variable "auto_scaling_group_name" {}
-variable "launch_configuration_name" {}
-variable "instance_type" {}
-variable "image_id" {}
 variable "key_name" {}
 variable "security_groups" {}
 variable "load_balancers" {}
-variable "min_size" {}
-variable "desired_capacity" {}
-variable "max_size" {}
+
+#variable "min_size" {}
+#variable "desired_capacity" {}
+#variable "max_size" {}
+variable "image_id" {}
+#variable "instance_type" {}
+
