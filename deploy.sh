8ab39a916412c3a9deb960314e0b6eb37e06a2d7
skohar <skohar@users.noreply.github.com>
2015-12-18 11:28:10 +0900

    Refactoring

--- deploy.sh
+++ deploy.sh
@@ -12,10 +12,7 @@ AMI_NAME=$(name)
 packer build -var "AMI_NAME=$AMI_NAME" packer.json
 RESULT=$?
 kill %1
-if [[ $RESULT != 0 ]]; then
-	exit $RESULT
-fi
-
+[ $RESULT != 0 ] && exit $RESULT
 
 if [[ ${1} == "whale" ]]; then
 	INSTANCE_TYPE='t2.medium'
@@ -37,12 +34,4 @@ fi
 
 AWS_DEFAULT_REGION='us-east-1' DATE=$(date -u +%FT%TZ | sed 's/://g' | sed 's/-//g') terraform apply -var "launch_configuration_name=$DATE" -var "auto_scaling_group_name=$DATE" -var "load_balancers=$LOAD_BALANCER" -var "instance_type=$INSTANCE_TYPE" -var "min_size=$MIN_SIZE" -var "desired_capacity=$DESIRED_CAPACITY" -var "max_size=$MAX_SIZE" -var "image_id=$AMI_NAME" -var-file=$VAR_FILE
 
-#instanceIds=$(aws ec2 run-instances --image-id $AMI_NAME --subnet-id $SUBNET_ID --instance-type $INSTANCE_TYPE --security-group-id $SECURITY_GROUP_ID --key-name $KEY_NAME --associate-public-ip-address --count $COUNT | jq -r '.Instances[] .InstanceId')
-#instanceIdBelongedElb=$(aws elb describe-load-balancers --load-balancer-names ${1} | jq -r '.LoadBalancerDescriptions[].Instances[].InstanceId')
-#aws ec2 create-tags --resources $instanceIds --tags Key=Name,Value=$INSTANCE_NAME
-#ipAddress=$(aws ec2 describe-instances --instance-ids $instanceIds | jq -r '.Reservations[].Instances[].PublicIpAddress')
-#aws elb register-instances-with-load-balancer --load-balancer-name ${1} --instances $instanceIds
-
-if [[ ${1} == "whale-dev" ]] then
-	aws elb deregister-instances-from-load-balancer --instances $instanceIdBelongedElb
-fi
+[ ${1} == "whale-dev" ] && aws elb deregister-instances-from-load-balancer --instances $instanceIdBelongedElb
