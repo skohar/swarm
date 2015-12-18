f7ea100fccb9088d739e1e6c373c84de0af0abe9
skohar <skohar@users.noreply.github.com>
2015-12-18 16:31:06 +0900

    Mod to confirm script working

--- deploy.sh
+++ deploy.sh
@@ -14,26 +14,27 @@ set -u
 #kill %1
 #[ $RESULT != 0 ] && exit $RESULT
 #
-#if [[ ${1} == "whale" ]]; then
-#	INSTANCE_TYPE='t2.medium'
-#	INSTANCE_NAME='whale'
-#	LOAD_BALANCER='whale'
-#	MIN_SIZE='2'
-#	DESIRED_CAPACITY='2'
-#	MAX_SIZE='5'
-#	VAR_FILE='terraform-prod.tfvars'
-#else
-#	INSTANCE_TYPE='t2.micro'
-#	INSTANCE_NAME='whale-dev'
-#	LOAD_BALANCER='whale-dev'
-#	MIN_SIZE='1'
-#	DESIRED_CAPACITY='1'
-#	MAX_SIZE='1'
-#	VAR_FILE='terraform-dev.tfvars'
-#fi
-#
-#AWS_DEFAULT_REGION='us-east-1'
-#DATE=$(date -u +%FT%TZ | sed 's/://g' | sed 's/-//g')
-#terraform apply -var "launch_configuration_name=$DATE" -var "auto_scaling_group_name=$DATE" -var "load_balancers=$LOAD_BALANCER" -var "instance_type=$INSTANCE_TYPE" -var "min_size=$MIN_SIZE" -var "desired_capacity=$DESIRED_CAPACITY" -var "max_size=$MAX_SIZE" -var "image_id=$AMI_NAME" -var-file=$VAR_FILE
-#
-#[ ${1} == "whale-dev" ] && aws elb deregister-instances-from-load-balancer --instances $instanceIdBelongedElb
+if [[ ${1} == "whale" ]]; then
+	INSTANCE_TYPE='t2.medium'
+	INSTANCE_NAME='whale'
+	LOAD_BALANCER='whale'
+	MIN_SIZE='2'
+	DESIRED_CAPACITY='2'
+	MAX_SIZE='5'
+	VAR_FILE='terraform-prod.tfvars'
+else
+	INSTANCE_TYPE='t2.micro'
+	INSTANCE_NAME='whale-dev'
+	LOAD_BALANCER='whale-dev'
+	MIN_SIZE='1'
+	DESIRED_CAPACITY='1'
+	MAX_SIZE='1'
+	VAR_FILE='terraform-dev.tfvars'
+fi
+
+AWS_DEFAULT_REGION='us-east-1'
+DATE=$(date -u +%FT%TZ | sed 's/://g' | sed 's/-//g')
+AMI_NAME="ami-a9e8abc3"
+terraform apply -var "launch_configuration_name=$DATE" -var "auto_scaling_group_name=$DATE" -var "load_balancers=$LOAD_BALANCER" -var "instance_type=$INSTANCE_TYPE" -var "min_size=$MIN_SIZE" -var "desired_capacity=$DESIRED_CAPACITY" -var "max_size=$MAX_SIZE" -var "image_id=$AMI_NAME" -var-file=$VAR_FILE
+
+[ ${1} == "whale-dev" ] && aws elb deregister-instances-from-load-balancer --instances $instanceIdBelongedElb
