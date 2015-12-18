91994d8f2f1ecd07802c65f5d2380372901237cb
skohar <skohar@users.noreply.github.com>
2015-12-18 10:55:40 +0900

    Fix syntax error

--- deploy.sh
+++ deploy.sh
@@ -18,15 +18,19 @@ fi
 
 
 if [[ ${1} == "whale" ]]; then
-	$INSTANCE_NAME='whale'
-	$LOAD_BALANCER='whale'
+	INSTANCE_TYPE='t2.medium'
+	INSTANCE_NAME='whale'
+	LOAD_BALANCER='whale'
+	MIN_SIZE='2'
+	DESIRED_CAPACITY='2'
+	MAX_SIZE='5'
 else
-	$INSTANCE_TYPE=$DEV_INSTANCE_TYPE
-	$INSTANCE_NAME='whale-dev'
-	$LOAD_BALANCER='whale-dev'
-	$MIN_SIZE='1'
-	$DESIRED_CAPACITY='1'
-	$MAX_SIZE='1'
+	INSTANCE_TYPE='t2.micro'
+	INSTANCE_NAME='whale-dev'
+	LOAD_BALANCER='whale-dev'
+	MIN_SIZE='1'
+	DESIRED_CAPACITY='1'
+	MAX_SIZE='1'
 fi
 
 AWS_DEFAULT_REGION='us-east-1' DATE=$(date -u +%FT%TZ | sed 's/://g' | sed 's/-//g') terraform apply -var "launch_configuration_name=$DATE" -var "auto_scaling_group_name=$DATE" -var "load_balancers=$LOAD_BALANCER" -var "instance_type=$INSTANCE_TYPE" -var "min_size=$MIN_SIZE" -var "desired_capacity=$DESIRED_CAPACITY" -var "max_size=$MAX_SIZE" -var "image_id=$AMI_NAME" -var-file=terraform-dev.tfvars
