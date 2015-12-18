68a132135a4c750e395f7ff8b94dbb4445763b92
skohar <skohar@users.noreply.github.com>
2015-12-18 16:54:31 +0900

    Fix script error

--- deploy.sh
+++ deploy.sh
@@ -21,7 +21,7 @@ if [[ ${1} == "whale" ]]; then
 	MIN_SIZE='2'
 	DESIRED_CAPACITY='2'
 	MAX_SIZE='5'
-	VAR_FILE='./terraform/attach/terraform-prod.tfvars'
+	VAR_FILE='terraform-prod.tfvars'
 else
 	INSTANCE_TYPE='t2.micro'
 	INSTANCE_NAME='whale-dev'
@@ -29,12 +29,12 @@ else
 	MIN_SIZE='1'
 	DESIRED_CAPACITY='1'
 	MAX_SIZE='1'
-	VAR_FILE='./terraform/attach/terraform-dev.tfvars'
+	VAR_FILE='terraform-dev.tfvars'
 fi
 
 AWS_DEFAULT_REGION='us-east-1'
 DATE=$(date -u +%FT%TZ | sed 's/://g' | sed 's/-//g')
 AMI_NAME="ami-a9e8abc3"
-terraform apply -var "launch_configuration_name=$DATE" -var "auto_scaling_group_name=$DATE" -var "load_balancers=$LOAD_BALANCER" -var "instance_type=$INSTANCE_TYPE" -var "min_size=$MIN_SIZE" -var "desired_capacity=$DESIRED_CAPACITY" -var "max_size=$MAX_SIZE" -var "image_id=$AMI_NAME" -var-file=$VAR_FILE
+cd terraform/attach && terraform apply -var "launch_configuration_name=$DATE" -var "auto_scaling_group_name=$DATE" -var "load_balancers=$LOAD_BALANCER" -var "instance_type=$INSTANCE_TYPE" -var "min_size=$MIN_SIZE" -var "desired_capacity=$DESIRED_CAPACITY" -var "max_size=$MAX_SIZE" -var "image_id=$AMI_NAME" -var-file=$VAR_FILE
 
 [ ${1} == "whale-dev" ] && aws elb deregister-instances-from-load-balancer --instances $instanceIdBelongedElb
