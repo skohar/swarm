21cf31375ac07a5863c8e130e758f8a7b99af79d
skohar <skohar@users.noreply.github.com>
2015-12-21 18:31:40 +0900

    Format

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -eux
 set +u
 if [[ ${1} == "" ]]; then
-	echo "Found no parameter"
-	exit 1
+    echo "Found no parameter"
+    exit 1
 fi
 set -u
 
@@ -16,21 +16,21 @@ RESULT=$?
 kill %1
 [ $RESULT != 0 ] && exit $RESULT
 if [[ ${1} == "whale" ]]; then
-	INSTANCE_TYPE='t2.medium'
-	INSTANCE_NAME='whale'
-	LOAD_BALANCER='whale'
-	MIN_SIZE='2'
-	DESIRED_CAPACITY='2'
-	MAX_SIZE='5'
-	VAR_FILE='terraform-prod.tfvars'
+    INSTANCE_TYPE='t2.medium'
+    INSTANCE_NAME='whale'
+    LOAD_BALANCER='whale'
+    MIN_SIZE='2'
+    DESIRED_CAPACITY='2'
+    MAX_SIZE='5'
+    VAR_FILE='terraform-prod.tfvars'
 else
-	INSTANCE_TYPE='t2.micro'
-	INSTANCE_NAME='whale-dev'
-	LOAD_BALANCER='whale-dev'
-	MIN_SIZE='1'
-	DESIRED_CAPACITY='1'
-	MAX_SIZE='1'
-	VAR_FILE='terraform-dev.tfvars'
+    INSTANCE_TYPE='t2.micro'
+    INSTANCE_NAME='whale-dev'
+    LOAD_BALANCER='whale-dev'
+    MIN_SIZE='1'
+    DESIRED_CAPACITY='1'
+    MAX_SIZE='1'
+    VAR_FILE='terraform-dev.tfvars'
 fi
 AWS_DEFAULT_REGION='us-east-1'
 DATE=$(date -u +%Y%m%dT%H%M%SZ)
