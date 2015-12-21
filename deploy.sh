431c8ac4c0a701789bce7a7043f518df7a4f4edb
skohar <skohar@users.noreply.github.com>
2015-12-21 10:45:20 +0900

    Packer build enable

--- deploy.sh
+++ deploy.sh
@@ -6,14 +6,13 @@ if [[ ${1} == "" ]]; then
 fi
 set -u
 
-#(while true; do echo "Packer is building AMI"; sleep 60; done) & # jobs %1
-#source ami_name.sh
-#AMI_NAME=$(name)
-#packer build -var "AMI_NAME=$AMI_NAME" packer.json
-#RESULT=$?
-#kill %1
-#[ $RESULT != 0 ] && exit $RESULT
-#
+(while true; do echo "Packer is building AMI"; sleep 60; done) & # jobs %1
+source ami_name.sh
+AMI_NAME=$(name)
+packer build -var "AMI_NAME=$AMI_NAME" packer.json
+RESULT=$?
+kill %1
+[ $RESULT != 0 ] && exit $RESULT
 if [[ ${1} == "whale" ]]; then
 	INSTANCE_TYPE='t2.medium'
 	INSTANCE_NAME='whale'
@@ -31,11 +30,8 @@ else
 	MAX_SIZE='1'
 	VAR_FILE='terraform-dev.tfvars'
 fi
-
 AWS_DEFAULT_REGION='us-east-1'
 DATE=$(date -u +%FT%TZ | sed 's/://g' | sed 's/-//g')
-AMI_NAME="ami-a9e8abc3"
 cd $HOME/whale-env/terraform/attach && terraform plan -var "launch_configuration_name=$DATE" -var "auto_scaling_group_name=$DATE" -var "load_balancers=$LOAD_BALANCER" -var "instance_type=$INSTANCE_TYPE" -var "min_size=$MIN_SIZE" -var "desired_capacity=$DESIRED_CAPACITY" -var "max_size=$MAX_SIZE" -var "image_id=$AMI_NAME" -var-file=$VAR_FILE
 cd $HOME/whale-env/terraform/attach && terraform apply -var "launch_configuration_name=$DATE" -var "auto_scaling_group_name=$DATE" -var "load_balancers=$LOAD_BALANCER" -var "instance_type=$INSTANCE_TYPE" -var "min_size=$MIN_SIZE" -var "desired_capacity=$DESIRED_CAPACITY" -var "max_size=$MAX_SIZE" -var "image_id=$AMI_NAME" -var-file=$VAR_FILE
 
-#[ ${1} == "whale-dev" ] && aws elb deregister-instances-from-load-balancer --instances $instanceIdBelongedElb
