cb25a71cfe6d5619cbdf49a6e0580973142d3ec5
skohar <skohar@users.noreply.github.com>
2016-01-24 18:19:16 +0900

    Rearrange code

--- generate_ami_name.sh
+++ generate_ami_name.sh
@@ -1,6 +1,6 @@
-#/bin/bash -eu
+#!/bin/bash -eux
 generate_ami_name () {
-    DATE_TIME=$(date -u +%Y%m%dT%H%M%SZ)
-    AMI_NAME=whale-$DATE_TIME-$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short HEAD)-$(cd env-builder && git checkout -q master && git rev-parse --abbrev-ref HEAD)-$(cd env-builder && git rev-parse --short HEAD)
-    echo $AMI_NAME
+  DATE_TIME=$(date -u +%Y%m%dT%H%M%SZ)
+  AMI_NAME=whale-$DATE_TIME-$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short HEAD)-$(cd env-builder && git checkout -q master && git rev-parse --abbrev-ref HEAD)-$(cd env-builder && git rev-parse --short HEAD)
+  echo $AMI_NAME
 }
