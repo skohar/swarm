a5b34b23141140b342e037f6ff9262cf6783e743
skohar <skohar@users.noreply.github.com>
2016-01-26 18:29:52 +0900

    Rearrange code

--- deploy-terraform.sh
+++ deploy-terraform.sh
@@ -1,4 +1,20 @@
 #!/bin/bash -eux
-current_dir=$PWD
-cd $current_dir/terraform/attach && terraform plan -var "launch_configuration_name=$DATE" -var "auto_scaling_group_name=$DATE" -var "load_balancers=$LOAD_BALANCER" -var "instance_type=$INSTANCE_TYPE" -var "min_size=$MIN_SIZE" -var "desired_capacity=$DESIRED_CAPACITY" -var "max_size=$MAX_SIZE" -var "image_id=$AMI_ID" -var-file=$VAR_FILE
-cd $current_dir/terraform/attach && terraform apply -var "launch_configuration_name=$DATE" -var "auto_scaling_group_name=$DATE" -var "load_balancers=$LOAD_BALANCER" -var "instance_type=$INSTANCE_TYPE" -var "min_size=$MIN_SIZE" -var "desired_capacity=$DESIRED_CAPACITY" -var "max_size=$MAX_SIZE" -var "image_id=$AMI_ID" -var-file=$VAR_FILE
+cd terraform/attach
+terraform plan -var "launch_configuration_name=$DATE" \
+  -var "auto_scaling_group_name=$DATE" \
+  -var "load_balancers=$LOAD_BALANCER" \
+  -var "instance_type=$INSTANCE_TYPE" \
+  -var "min_size=$MIN_SIZE" \
+  -var "desired_capacity=$DESIRED_CAPACITY" \
+  -var "max_size=$MAX_SIZE" \
+  -var "image_id=$AMI_ID" \
+  -var-file=$VAR_FILE
+terraform apply -var "launch_configuration_name=$DATE" \
+  -var "auto_scaling_group_name=$DATE" \
+  -var "load_balancers=$LOAD_BALANCER" \
+  -var "instance_type=$INSTANCE_TYPE" \
+  -var "min_size=$MIN_SIZE" \
+  -var "desired_capacity=$DESIRED_CAPACITY" \
+  -var "max_size=$MAX_SIZE" \
+  -var "image_id=$AMI_ID" \
+  -var-file=$VAR_FILE
