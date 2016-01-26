bf476d4a628b9a49ea4c6835c79b7d9cb1c1c67a
skohar <skohar@users.noreply.github.com>
2016-01-26 17:28:41 +0900

    Fix complex packer deployment script

--- deploy-packer.sh
+++ deploy-packer.sh
@@ -1,13 +1,5 @@
 #!/bin/bash -eux
 AMI_NAME=${1}
-(while true; do echo "Packer is building AMI"; sleep 60; done) & # jobs %1
 packer build -machine-readable -var "AMI_NAME=$AMI_NAME" packer.json > packer-build.log
-RESULT=$?
-kill %1
-if [[ $RESULT == 0 ]]; then
-  # parse 'amazon-ebs: AMIs were created: us-east-1: ami-6cdc8006'
-  AMI_ID=$(tail -1 packer-build.log | awk '{print $6}')
-  echo $AMI_ID
-else
-  exit $RESULT
-fi
+AMI_ID=$(tail -1 packer-build.log | awk '{print $6}') # parse 'amazon-ebs: AMIs were created: us-east-1: ami-6cdc8006'
+echo $AMI_ID
