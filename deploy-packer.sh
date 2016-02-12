95cc7e6ed030c75840609b38556759f68dd23f54
skohar <skohar@users.noreply.github.com>
2016-02-12 12:05:12 +0900

    Output packer build log

--- deploy-packer.sh
+++ deploy-packer.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -eux
 AMI_NAME=${1}
 packer build -machine-readable -var "AMI_NAME=$AMI_NAME" packer.json > packer-build.log
+cat packer-build.log
 AMI_ID=$(tail -1 packer-build.log | awk '{print $6}') # parse 'amazon-ebs: AMIs were created: us-east-1: ami-6cdc8006'
 echo $AMI_ID
